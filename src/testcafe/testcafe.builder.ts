df92cf6283fafde517d213b908b250aacd67bc31
Tim Nederhoff <post@timnederhoff.nl>
2019-06-04 10:20:11 +0200

    changes for angular 8: https://blog.angularindepth.com/angular-cli-under-the-hood-builders-demystified-v2-e73ee0f2d811

--- src/testcafe/testcafe.builder.ts
+++ src/testcafe/testcafe.builder.ts
@@ -1,126 +1,26 @@
 import {
-  Builder,
-  BuilderConfiguration,
   BuilderContext,
-  BuilderDescription,
-  BuildEvent
+  BuilderOutput,
+  createBuilder,
+  targetFromTargetString,
 } from '@angular-devkit/architect';
-import { Observable, of, from } from 'rxjs';
-import { concatMap, take, tap, map } from 'rxjs/operators';
-import { DevServerBuilderOptions } from '@angular-devkit/build-angular';
+import { JsonObject, tags } from '@angular-devkit/core';
 import * as url from 'url';
 import { isMatch } from 'lodash';
+import { TestcafeBuilderOptions } from './schema';
 
-export interface TestcafeBuilderSchema {
-  devServerTarget?: string;
-  src: string;
-  browsers: string[];
-  host: string;
-  port?: number;
-  reporters: Reporter[];
-  concurrency?: number;
-  screenshotsPath?: string;
-  screenshotsOnFails?: boolean;
-  screenshotsPathPattern?: string;
-  quarantineMode?: boolean;
-  debugMode?: boolean;
-  skipJsErrors?: boolean;
-  skipUncaughtErrors?: boolean;
-  debugOnFail?: boolean;
-  selectorTimeout?: number;
-  assertionTimeout?: number;
-  pageLoadTimeout?: number;
-  speed?: number;
-  ports?: number[];
-  proxy?: string;
-  proxyBypass?: string[];
-  disablePageReloads?: boolean;
-  dev?: boolean;
-  stopOnFirstFail?: boolean;
-  disableTestSyntaxValidation?: boolean;
-  color?: boolean;
-  NoColor?: boolean;
-  ssl?: string;
-  test?: string;
-  testGrep?: string;
-  testMeta?: object;
-  fixture?: string;
-  fixtureGrep?: string;
-  fixtureMeta?: object;
-}
+//TODO: change code to fit angular 8: https://blog.angularindepth.com/angular-cli-under-the-hood-builders-demystified-v2-e73ee0f2d811
 
-export interface Reporter {
-  name: string,
-  output?: string;
-}
-
-export default class TestcafeBuilder implements Builder<TestcafeBuilderSchema> {
-
-  constructor(public context: BuilderContext) { }
-
-  run(builderConfig: BuilderConfiguration<TestcafeBuilderSchema>): Observable<BuildEvent> {
-
-    const options = builderConfig.options;
-
-    return of(null).pipe(
-      concatMap(() => options.devServerTarget ? this._startDevServer(options) : of(null)),
-      concatMap(() => this._runTestcafe(options)),
-      take(1),
-    );
-  }
-
-  // Note: this method mutates the options argument.
-  private _startDevServer(options: TestcafeBuilderSchema) {
-    const architect = this.context.architect;
-    const [project, targetName, configuration] = (options.devServerTarget as string).split(':');
-    // Override dev server watch setting.
-    const overrides: Partial<DevServerBuilderOptions> = { watch: false };
-    if (options.host !== undefined) { overrides.host = options.host; }
-    if (options.port !== undefined) { overrides.port = options.port; }
-    const targetSpec = { project, target: targetName, configuration, overrides };
-    const builderConfig = architect.getBuilderConfiguration<DevServerBuilderOptions>(targetSpec);
-    let devServerDescription: BuilderDescription;
-    let baseUrl: string;
-
-    return architect.getBuilderDescription(builderConfig).pipe(
-      tap(description => devServerDescription = description),
-      concatMap(devServerDescription => architect.validateBuilderOptions(
-        builderConfig, devServerDescription)),
-      concatMap(() => {
-        // Compute baseUrl from devServerOptions.
-        if (options.devServerTarget && builderConfig.options.publicHost) {
-          let publicHost = builderConfig.options.publicHost;
-          if (!/^\w+:\/\//.test(publicHost)) {
-            publicHost = `${builderConfig.options.ssl
-              ? 'https'
-              : 'http'}://${publicHost}`;
-          }
-          const clientUrl = url.parse(publicHost);
-          baseUrl = url.format(clientUrl);
-        } else if (options.devServerTarget) {
-          baseUrl = url.format({
-            protocol: builderConfig.options.ssl ? 'https' : 'http',
-            hostname: options.host,
-            port: builderConfig.options.port.toString(),
-          });
-        }
-
-        return of(this.context.architect.getBuilder(devServerDescription, this.context));
-      }),
-      concatMap(builder => builder.run(builderConfig)),
-    );
-  }
-
-  private _runTestcafe(options: TestcafeBuilderSchema): Observable<BuildEvent> {
-    const port1 = options.ports && options.ports[0];
-    const port2 = options.ports && options.ports[1];
-    const concurrency = options.concurrency || 1;
-    const externalProxyHost = options.proxy;
-    const proxyBypass = options.proxyBypass;
-    const createTestCafe = require('testcafe');
-    return from(createTestCafe(options.host, port1, port2, options.ssl, options.dev).then((testCafe) => {
-      const runner = testCafe.createRunner();
-      return runner
+function runTestcafe(root: String, options: TestcafeBuilderOptions): Promise<BuilderOutput> {
+  const port1 = options.ports && options.ports[0];
+  const port2 = options.ports && options.ports[1];
+  const concurrency = options.concurrency || 1;
+  const externalProxyHost = options.proxy;
+  const proxyBypass = options.proxyBypass;
+  const createTestCafe = require('testcafe');
+  return createTestCafe(options.host, port1, port2, options.ssl, options.dev).then((testCafe) => {
+    const runner = testCafe.createRunner();
+    return runner
         .useProxy(externalProxyHost, proxyBypass)
         .src(options.src)
         .browsers(options.browsers)
@@ -158,6 +58,77 @@ export default class TestcafeBuilder implements Builder<TestcafeBuilderSchema> {
           testCafe.close();
           process.exit(1);
         });
-    })).pipe(map(() => ({success: true})));
+  }) as Promise<BuilderOutput>;
+}
+
+export async function execute(
+    options: TestcafeBuilderOptions,
+    context: BuilderContext,
+): Promise<BuilderOutput> {
+  // ensure that only one of these options is used
+  if (options.devServerTarget && options.baseUrl) {
+    throw new Error(tags.stripIndents`
+    The 'baseUrl' option cannot be used with 'devServerTarget'.
+    When present, 'devServerTarget' will be used to automatically setup 'baseUrl' for Protractor.
+    `);
+  }
+
+  let baseUrl;
+  let server;
+  if (options.devServerTarget) {
+    const target = targetFromTargetString(options.devServerTarget);
+    const serverOptions = await context.getTargetOptions(target);
+
+    const overrides: Record<string, string | number | boolean> = { watch: false };
+    if (options.host !== undefined) {
+      overrides.host = options.host;
+    } else if (typeof serverOptions.host === 'string') {
+      options.host = serverOptions.host;
+    } else {
+      options.host = overrides.host = 'localhost';
+    }
+
+    if (options.port !== undefined) {
+      overrides.port = options.port;
+    } else if (typeof serverOptions.port === 'number') {
+      options.port = serverOptions.port;
+    }
+
+    server = await context.scheduleTarget(target, overrides);
+    const result = await server.result;
+    if (!result.success) {
+      return { success: false };
+    }
+
+    if (typeof serverOptions.publicHost === 'string') {
+      let publicHost = serverOptions.publicHost as string;
+      if (!/^\w+:\/\//.test(publicHost)) {
+        publicHost = `${serverOptions.ssl
+            ? 'https'
+            : 'http'}://${publicHost}`;
+      }
+      const clientUrl = url.parse(publicHost);
+      baseUrl = url.format(clientUrl);
+    } else if (typeof result.baseUrl === 'string') {
+      baseUrl = result.baseUrl;
+    } else if (typeof result.port === 'number') {
+      baseUrl = url.format({
+        protocol: serverOptions.ssl ? 'https' : 'http',
+        hostname: options.host,
+        port: result.port.toString(),
+      });
+    }
+  }
+
+  try {
+    return await runTestcafe(context.workspaceRoot, options);
+  } catch {
+    return { success: false };
+  } finally {
+    if (server) {
+      await server.stop();
+    }
   }
 }
+
+export default createBuilder<JsonObject & TestcafeBuilderOptions>(execute);
