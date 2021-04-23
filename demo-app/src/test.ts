76cf3d33947a22cbe2c2e47b05b7871891472ec5
Andrey <andrey.belym@devexpress.com>
2021-04-23 15:55:56 +0300

    fix: elimante the 'any' warning

--- demo-app/src/test.ts
+++ demo-app/src/test.ts
@@ -1,13 +1,13 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+// NOTE: This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+import 'webpack-env';
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
+
 import {
     BrowserDynamicTestingModule,
     platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-
-declare const require: any;
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
